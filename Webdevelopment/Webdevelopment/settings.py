42d7d429ad4173f7ddf12c8a15894c22948ca599
koutilyagurjikindi <koutilyagurjikindi@gmail.com>
2020-07-05 07:08:48 +0530

    changes

--- Webdevelopment/Webdevelopment/settings.py
+++ Webdevelopment/Webdevelopment/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'scrumboard'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,10 @@ WSGI_APPLICATION = 'Webdevelopment.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'webdevelopment',
+        'USER': os.environ.get('MYSQL_USERNAME', 'web'),
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD', 'web'),
     }
 }
 
